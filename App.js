21b884ebd1d9bdfbe9694efdd5280d76fb8c2a06
Ryan Camp <campryan@comcast.net>
2020-07-18 11:06:04 -0600

    Move styles to its own folder

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styles from './styles/styles'
 
 export default function App() {
   return (
@@ -10,12 +11,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+let styles;
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
