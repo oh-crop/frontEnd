995b31254d90e7da1ba1ef61893212dfc77f6ab5
Ryan Camp <campryan@comcast.net>
2020-07-18 13:00:09 -0600

    Add text, logo, and button to stylesheet

--- styles/styles.js
+++ styles/styles.js
@@ -8,4 +8,21 @@ export default styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+    fontSize: 20,
+  },
+  logo: {
+    height: 300,
+    width: 300,
+  },
+  button: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#5DA25B',
+    boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.05)",
+    margin: 10,
+    width: 150,
+    textAlign: 'center'
+  }
 });
