d5bc3ab3d843b5052c3b8d6b83622aa51495a6eb
Ryan Camp <campryan@comcast.net>
2020-07-18 12:58:22 -0600

    Gut App.js and move functionality to components

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import styles from './styles/styles'
+import Landing from './components/pages/Landing'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+      <Landing />
+    // </View>
   );
 }
