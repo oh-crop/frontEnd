dea5a1745138bffa67f919af8541c5afd23ab11a
Ryan Camp <campryan@comcast.net>
2020-07-18 12:57:03 -0600

    Create landing page component

---
+++ components/pages/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TouchableOpacity, View, Image, Text } from 'react-native';
+import styles from '../../styles/styles';
+
+export default function Landing() {
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('../../assets/logo.png')} style={styles.logo} />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Search Plants</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>My Garden</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
