49951bc2530150510fea23f197254e579dafcdb2
Madison Randle <madisonrandle25@gmail.com>
2020-07-20 12:26:09 -0500

    Add onPress property and call on navigation props

--- components/pages/Landing.js
+++ components/pages/Landing.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { TouchableOpacity, View, Image, Text } from 'react-native';
 import styles from '../../styles/styles';
 
-export default function Landing() {
+export default function Landing(props) {
 
   return (
     <View style={styles.container}>
       <Image source={require('../../assets/logo.png')} style={styles.logo} />
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={ () => props.navigation.navigate('Search') }>
         <Text style={styles.text}>Search Plants</Text>
       </TouchableOpacity>
 
