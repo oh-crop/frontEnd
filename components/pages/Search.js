0daa475cc70bb35b3151f21a99c3d6adc48f67b9
Madison Randle <madisonrandle25@gmail.com>
2020-07-20 13:00:45 -0500

    Add space within imports to organize imports

--- components/pages/Search.js
+++ components/pages/Search.js
@@ -4,7 +4,6 @@ import { View, Text } from 'react-native';
 import styles from '../../styles/styles';
 
 export default function Search() {
-  console.log('here')
   return (
     <View style={styles.container}>
       <Text style={{color: 'black'}}>
