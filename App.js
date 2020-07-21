d53748c6ee7cd8d2691c1e16727ed125f2ae93eb
Ryan Camp <campryan@comcast.net>
2020-07-20 19:50:15 -0600

    Change navigation flow to bottom tabs instead of stack

--- App.js
+++ App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import styles from './styles/styles';
 import Landing from './components/pages/Landing';
 import Search from './components/pages/Search';
 import MyGarden from './components/pages/MyGarden';
 
-const Stack = createStackNavigator();
+const Tab =  createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Landing} options={{ title: 'Welcome' }} />
-        <Stack.Screen name="Search" component={Search} options={{ title: 'Search Screen' }}/>
-        <Stack.Screen name="MyGarden" component={MyGarden} options={{ title: 'My Garden Screen' }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Home" >
+          <Tab.Screen name="Home" component={Landing} options={{ title: 'HomeScreen', tabBarVisible: false }} />
+          <Tab.Screen name="Search" component={Search} options={{ title: 'Search Screen' }}/>
+          <Tab.Screen name="MyGarden" component={MyGarden} options={{ title: 'My Garden Screen' }}/>
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
