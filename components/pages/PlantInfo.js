eeb68eeb6eb973a9ab46f3450fe9e4590b3363e7
Ryan Camp <campryan@comcast.net>
2020-07-22 17:41:08 -0600

    Create Plant info page

---
+++ components/pages/PlantInfo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from '../../styles/styles';
+
+export default function PlantInfo() {
+  return (
+    <View>
+      <Text>PlantInfo Component</Text>
+    </View>
+  )
+}
