b12019e49279e2c95a7b5cf8ee68d9b552794212
Ryan Camp <campryan@comcast.net>
2020-07-22 15:31:40 -0600

    Switch to using a nested navigation technique tab within stack

--- components/pages/Landing.js
+++ components/pages/Landing.js
@@ -9,14 +9,13 @@ export default function Landing(props) {
     <View style={styles.container}>
       <Image source={require('../../assets/logo.png')} style={styles.logo} />
 
-      <TouchableOpacity style={styles.button} onPress={ () => props.navigation.navigate('Search') }>
+      <TouchableOpacity style={styles.button} onPress={ () => props.navigation.navigate('TabNavigation', { screen: 'Search'}) }>
         <Text style={styles.text}>Search Plants</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} onPress={ () => props.navigation.navigate('MyGarden') }>
+      <TouchableOpacity style={styles.button} onPress={ () => props.navigation.navigate('TabNavigation', { screen: 'Garden' }) }>
         <Text style={styles.text}>My Garden</Text>
       </TouchableOpacity>
-
     </View>
   );
 }
