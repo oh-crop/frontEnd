c3fef6322e1ba6f1b0eb99d5086f91be08f66e6f
Ryan Camp <campryan@comcast.net>
2020-07-22 17:27:38 -0600

    Remove unnecessary imports and remove header from stack

--- App.js
+++ App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import styles from './styles/styles';
 import TabNavigation from './components/navigation/TabNavigation'
 import Landing from './components/pages/Landing'
-// import Search from './components/pages/Search'
-// import MyGarden from './components/pages/MyGarden'
+
 
 const Stack = createStackNavigator();
 
-// <Tab.Screen name="Home" component={Landing} options={{ tabBarVisible: false, tabBarIcon: () => homeIcon }} />
 export default function App() {
   return (
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
           >
           <Stack.Screen name="Home" component={Landing} />
           <Stack.Screen name="TabNavigation" component={TabNavigation} />
