e09f46a798d9ba2925c835709e07c15ed503a402
Ryan Camp <campryan@comcast.net>
2020-07-22 14:01:55 -0600

    Modify state fake data easier to read

--- components/pages/Search.js
+++ components/pages/Search.js
@@ -8,19 +8,21 @@ import styles from '../../styles/styles';
 
 export default class Search extends Component {
   state = {
-    searchResults: [ {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },],
-    hasSearched: true
+    searchResults: [
+      {
+      id: "1",
+      title: "First Item",
+      },
+      {
+      id: "2",
+      title: "Second Item",
+      },
+      {
+      id: "3",
+      title: "Third Item",
+      },
+    ],
+    hasSearched: false,
   };
 
   meetThePlant = () => (
