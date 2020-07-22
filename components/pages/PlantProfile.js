38dfe68f357c8bbcba2b1e30c1f2e4487ce3f766
Ryan Camp <campryan@comcast.net>
2020-07-22 17:43:32 -0600

    Create PlantProfile Page

---
+++ components/pages/PlantProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from '../../styles/styles';
+
+export default function PlantProfile() {
+  return (
+    <View>
+      <Text>PlantProfile Component</Text>
+    </View>
+  )
+}
