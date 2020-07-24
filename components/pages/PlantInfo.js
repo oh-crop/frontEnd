e80e9e8ed9e3c004cc80ab2ae279c77b7cbe6117
Madison Randle <madisonrandle25@gmail.com>
2020-07-24 18:02:45 -0500

    Update code indentation

--- components/pages/PlantInfo.js
+++ components/pages/PlantInfo.js
@@ -19,51 +19,31 @@ export default function PlantInfo({ navigation }) {
         source={backgroundImg}
         style={styles.backgroundImg}
         >
-
         <View style={styles.plantInfoHeader}>
-          <Text style={styles.plantName}>
-            Sunflower
-          </Text>
+          <Text style={styles.plantName}>Sunflower</Text>
         </View>
-
         <View style={styles.plantImgWrapper}></View>
         <View style={styles.plantImgContainer}>
           <Image
             style={styles.plantImg}
             source={{uri: sunflowerImg}}/>
         </View>
-        
         <View style={styles.plantInfoContentContainer}>
           <View style={styles.plantInfoContent}>
-            <Text style={styles.plantAttrLabel}>
-              Lighting:
-              <Text style={styles.plantAttrValue}>
-                Full Sun
-              </Text>
+            <Text style={styles.plantAttrLabel}>Lighting:
+              <Text style={styles.plantAttrValue}>Full Sun</Text>
             </Text>
-            <Text style={styles.plantAttrLabel}>
-              How Often To Water:
-              <Text style={styles.plantAttrValue}>
-                Every 2 days
-              </Text>
+            <Text style={styles.plantAttrLabel}>How Often To Water:
+              <Text style={styles.plantAttrValue}>Every 2 days</Text>
             </Text>
-            <Text style={styles.plantAttrLabel}>
-              Seed To Harvest:
-              <Text style={styles.plantAttrValue}>
-                70 Days
-              </Text>
+            <Text style={styles.plantAttrLabel}>Seed To Harvest:
+              <Text style={styles.plantAttrValue}>70 Days</Text>
             </Text>
-            <Text style={styles.plantAttrLabel}>
-              Root Depth:
-              <Text style={styles.plantAttrValue}>
-                2 Inches
-              </Text>
+            <Text style={styles.plantAttrLabel}>Root Depth:
+              <Text style={styles.plantAttrValue}>2 Inches</Text>
             </Text>
-            <Text style={styles.plantAttrLabel}>
-              Perennial/Annual:
-              <Text style={styles.plantAttrValue}>
-                Annual
-              </Text>
+            <Text style={styles.plantAttrLabel}>Perennial/Annual:
+              <Text style={styles.plantAttrValue}>Annual</Text>
             </Text>
           </View>
         </View>
@@ -74,9 +54,7 @@ export default function PlantInfo({ navigation }) {
             navigation.navigate('SearchPage')}
           }
           >
-          <Text style={styles.goBackButton}>
-            Go Back to Search
-          </Text>
+          <Text style={styles.goBackButton}>Go Back to Search</Text>
         </TouchableOpacity>
       </ImageBackground>
     </SafeAreaView>
