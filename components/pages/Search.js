2c61a415a8bdf0087d2e04775d4a24fbc16a4252
Ryan Camp <campryan@comcast.net>
2020-07-24 16:57:08 -0600

    Implement http requests for random plant and search

--- components/pages/Search.js
+++ components/pages/Search.js
@@ -1,66 +1,98 @@
 import React, {Component} from 'react';
-import { View, Text, FlatList, SafeAreaView } from 'react-native';
+import { View, Text, FlatList, SafeAreaView, Scrollview } from 'react-native';
 
 import PlantItem from '../PlantItem';
 import ImagePlaceholder from '../../assets/meet-a-plant-example.jpg';
 import SearchBar from '../SearchBar';
 import styles from '../../styles/styles';
+import api from '../../api/plantAPI'
 
 export default class Search extends Component {
+
   state = {
-    searchResults: [
-      {
-      id: "1",
-      title: "First Item",
-      },
-      {
-      id: "2",
-      title: "Second Item",
-      },
-      {
-      id: "3",
-      title: "Third Item",
-      },
-    ],
+    searchResults: [],
+    randomPlant: {},
     hasSearched: true,
   };
+
+  componentDidMount() {
+    this.gatherInfo('tomato')
+  }
 
   render () {
     return (
       <SafeAreaView>
         <SearchBar />
-        {!this.state.hasSearched
+        { !this.state.hasSearched
           ? this.meetThePlant()
-          : this.searchResults()}
-        </SafeAreaView>
-      )
-    }
-// onPress={() => alert("You have pressed my buttons!")
+          : this.searchResults() }
+      </SafeAreaView>
+    )
+  }
+
+  // this method will render to the page if the 'hasSearched' toggle is set to false
   meetThePlant = () => (
     <View style={styles.container}>
       <Text>MEET A NEW PLANT!</Text>
       <PlantItem
-        title={'Plant1'}
-        image={ImagePlaceholder}
+        title={this.state.randomPlant.plant_type}
+        image={this.state.randomPlant.plant_image}
         searchNavigation={this.props.navigation}
         />
     </View>
   )
 
   searchResults = () => (
-    <View style={styles.container}>
-      <FlatList
-        style={styles.searchResultsContainer}
-        data={this.state.searchResults}
-        renderItem={({item, index, separators}) => (
-          <PlantItem
-            title={item.title}
-            image={ImagePlaceholder}
-            searchNavigation={this.props.navigation}
-          />
-        )}
+    <FlatList
+      style={styles.searchResultsContainer}
+      data={this.state.searchResults}
+      keyExtractor={item => `${item.id}`}
+      renderItem={this.renderPlantItem}
       />
-    </View>
   )
+
+  renderPlantItem = ({ item }) => (
+    <PlantItem
+      title={item.plant_type}
+      image={item.plant_image}
+      searchNavigation={this.props.navigation}
+    />
+  )
+
+  // this method will render to the page if the 'hasSearched' toggle is set to false
+
+  //We should call this before return in the render to populate the info we need
+  gatherInfo = (query) => {
+    if (this.state.hasSearched) {
+      //this argument needs to be made dynamic by what is coming through the search bar.
+      this.getSearchResults(query)
+    } else {
+      this.getRandomPlant()
+    }
+  }
+
+  // axios call that gets a random plant... Should be abstracted from here     ....This is old now, the way I did it before abstraction....
+  getRandomPlant = () => {
+    api.getRandomPlant()
+    .then(response => {
+      this.setState({ randomPlant: response.data, hasSearched: false } )
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  // axios call that will get search results when those are available... Should also be abstracted from here.
+  getSearchResults = (query) => {
+    api.getPlantBySearch(query)
+    .then(response => {
+      this.setState({ searchResults: [...response.data], hasSearched: true})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+
 
 }
