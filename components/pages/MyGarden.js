54f6c838efa81ade71c9f40412895f5e2fa1b95f
Ryan Camp <campryan@comcast.net>
2020-07-26 19:14:21 -0600

    Modify MyGarden to use hooks

--- components/pages/MyGarden.js
+++ components/pages/MyGarden.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { View, Text, ImageBackground, SafeAreaView, ScrollView} from 'react-native';
 
 import GardenPlant from '../GardenPlant';
@@ -6,45 +6,47 @@ import styles from '../../styles/styles';
 import dirtBackground from '../../assets/dirt.png';
 import api from '../../api/plantAPI'
 
-export default class MyGarden extends Component {
-  state = {
-    gardenPlants: [],
-  }
+export default function ({ navigation }) {
+  const [gardenPlants, setGardenPlants] = useState([])
 
-  componentDidMount() {
-    this.getAllGardenPlants()
-  }
+  React.useEffect(() => {
+     const updatePlants = navigation.addListener('focus', () => {
+       getAllGardenPlants()
+       console.log('here here')
+     });
 
-  getAllGardenPlants = () => {
+     return updatePlants;
+   }, [navigation]);
+
+  const getAllGardenPlants = () => {
     api.getAllGardenPlants()
     .then(response => {
-      this.setState({gardenPlants: response.data})
+      setGardenPlants(response.data)
     })
     .catch(err => console.log(err))
   }
 
-  render(){
-    return (
-      <SafeAreaView style={styles.myGardenContainer}>
-        <ImageBackground
-          style={styles.dirtBackground}
-          source={dirtBackground}>
-          <View style={styles.myGardenContainer}>
-            <View style={styles.myGardenHeader}>
-              <Text style={{color: 'white'}}>My Garden</Text>
-            </View>
-            <ScrollView >
-              <View style={styles.myGarden}>
-                {this.state.gardenPlants.map( plant => {
-                  return(
-                    <GardenPlant navigation={ this.props.navigation } info={plant} key={plant.id}/>
-                  )
-                })}
-              </View>
-            </ScrollView>
+
+  return (
+    <SafeAreaView style={styles.myGardenContainer}>
+      <ImageBackground
+        style={styles.dirtBackground}
+        source={dirtBackground}>
+        <View style={styles.myGardenContainer}>
+          <View style={styles.myGardenHeader}>
+            <Text style={{color: 'white'}}>My Garden</Text>
           </View>
-        </ImageBackground>
-      </SafeAreaView>
-    );
-  }
+          <ScrollView >
+            <View style={styles.myGarden}>
+              {gardenPlants.map( plant => {
+                return(
+                  <GardenPlant navigation={ navigation } info={plant} key={plant.id}/>
+                )
+              })}
+            </View>
+          </ScrollView>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  )
 }
