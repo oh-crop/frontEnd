b48379208caa9d6ddb2b6b7d322155adb0780b22
Madison Randle <madisonrandle25@gmail.com>
2020-07-26 21:01:39 -0500

    Change PlantItem wrapper from TouchableHighlight to TouchableOpacity

--- components/PlantItem.js
+++ components/PlantItem.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { View, Text, Image, TouchableHighlight} from 'react-native';
+import { View, Text, Image, TouchableOpacity} from 'react-native';
 
 import styles from '../styles/styles';
 
 export default function PlantItem ({ title, image, navigation, id }) {
   return (
-    <TouchableHighlight
+    <TouchableOpacity
       onPress={() => {
         navigation.navigate('PlantInfoPage', {id: id});
         navigation.dangerouslyGetParent().setOptions({ tabBarVisible: false });
       }} >
-  
+
       <View>
         <Image style={styles.meetAPlant} source={{uri: image}}/>
         <Text>{ title }</Text>
       </View>
-    </TouchableHighlight>
+    </TouchableOpacity>
   );
 }
