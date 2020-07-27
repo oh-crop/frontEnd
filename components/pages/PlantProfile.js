7acb52ee9e0120fd3293f1c8f4e4701b6543ba9c
Madison Randle <madisonrandle25@gmail.com>
2020-07-27 11:18:30 -0500

    Move water and remove button rendering above plant attributes

--- components/pages/PlantProfile.js
+++ components/pages/PlantProfile.js
@@ -3,7 +3,6 @@ import {
   View,
   Text,
   SafeAreaView,
-  Image,
   TouchableOpacity,
   ImageBackground,
   Button
@@ -46,7 +45,7 @@ export default class PlantProfile extends Component {
   waterPlant = (id) => {
     api.waterPlant(id)
     .then(response => {
-      let plantInfo = this.state.plantInfo 
+      let plantInfo = this.state.plantInfo
       plantInfo.last_watered = response.data.last_watered
       plantInfo.days_until_next_water = response.data.water_frequency
       this.setState({plantInfo: plantInfo})
@@ -58,41 +57,53 @@ export default class PlantProfile extends Component {
     // if(!this.state.isLoaded) {
     //   return <Text>LOADING</Text>
     // }
-    
+
     return (
       <SafeAreaView style={styles.container}>
         <ImageBackground source={backgroundImg} style={styles.greenCropBackground}>
           <View style={styles.plantInfoHeader}>
-            <Text style={styles.plantChildName}>{this.state.plantInfo.plant_name}</Text>
+            <Text style={styles.headerText}>{this.state.plantInfo.plant_name}</Text>
             <Text style={styles.plantName}>{this.state.plantInfo.plant_type}</Text>
           </View>
           <View style={styles.transparentSubHeader}></View>
           <View style={styles.plantImgContainer}>
-          <Button
-            onPress={() => this.waterPlant(this.state.plantInfo.gardenplant_id)}
-            title={"Click when you've watered"} />
-          {/* this image was removed and the icon was added - we can put the picture back once the image comes from API*/}
+            {/* this image was removed and the icon was added - we can put the picture back once the image comes from API*/}
             <MaterialCommunityIcons
-              style={styles.icon}
+              style={[styles.icon]}
               name="flower"
               size={40} />
           </View>
           <View style={styles.plantContentContainer}>
-            <View style={styles.plantChoresContent}>
-              <Text style={styles.plantAttrLabel}>Last Watered On:</Text>
-              <Text style={styles.plantAttrValue}>{this.state.plantInfo.last_watered}</Text>
-              {/* placeholder below - will be adding this info from the API once it gets updated*/}
-              <Text style={styles.plantAttrLabel}>Next Water in:</Text>
-              <Text style={styles.plantAttrValue}>5 Days</Text>
-              <Text style={styles.plantAttrLabel}>Harvest Date:</Text>
-              <Text style={styles.plantAttrValue}>{this.state.plantInfo.harvest_date}</Text>
-              {/* placeholder below - will be adding this info from the API once it gets updated*/}
-              <Text style={styles.plantAttrLabel}>Harvest in:</Text>
-              <Text style={styles.plantAttrValue}>{this.state.plantInfo.days_until_harvest}</Text>
+            <View style={[styles.plantProfileContainer, styles.borderRadius]}>
+              <View style={styles.plantActionsContainer}>
+                <Button
+                  onPress={() => this.waterPlant(this.state.plantInfo.gardenplant_id)}
+                  title={'Water'}
+                  accessibilityLabel={'Click to water your plant'}
+                  style={styles.water}
+                />
+                <Button
+                  onPress={() => this.deletePlant(this.state.plantInfo.gardenplant_id)}
+                  title={'Remove'}
+                  accessibilityLabel={'Click to remove plant from your garden'}
+                  style={styles.remove}
+                />
+              </View>
+              <View style={styles.plantChoresContent}>
+                <Text style={styles.plantProfileAttrLabel}>
+                Last Watered On:
+                </Text>
+                <Text style={styles.plantProfileAttrValue}>{this.state.plantInfo.last_watered}</Text>
+                {/* placeholder below - will be adding this info from the API once it gets updated*/}
+                <Text style={styles.plantProfileAttrLabel}>Next Water in:</Text>
+                <Text style={styles.plantProfileAttrValue}>5 Days</Text>
+                <Text style={styles.plantProfileAttrLabel}>Harvest Date:</Text>
+                <Text style={styles.plantProfileAttrValue}>{this.state.plantInfo.harvest_date}</Text>
+                {/* placeholder below - will be adding this info from the API once it gets updated*/}
+                <Text style={styles.plantProfileAttrLabel}>Harvest in:</Text>
+                <Text style={styles.plantProfileAttrValue}>{this.state.plantInfo.days_until_harvest}</Text>
             </View>
-            <Button
-              onPress={() => this.deletePlant(this.state.plantInfo.gardenplant_id)}
-              title={'Click to delete your plant from the garden'} />
+            </View>
           </View>
           <TouchableOpacity
             style={styles.backButtonContainer}
