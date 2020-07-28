d6276f23b69a2140b156ca8ccfcd60e93b881582
Madison Randle <madisonrandle25@gmail.com>
2020-07-28 16:12:51 -0500

    Update stylying properties to render namePlant page

--- components/InputBar.js
+++ components/InputBar.js
@@ -1,28 +1,30 @@
 import React, {Component, useState} from 'react';
-import { View, Text, Button, TextInput,   TouchableOpacity,
- } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import styles from '../styles/styles';
-
 const searchIcon = <Ionicons name="md-search" size={40} color="black" />
 
 function InputBar (props) {
-
   return (
     <View style={props.styles}>
       <TextInput
         style={[styles.addNameSearchbox, styles.textDark]}
-          placeholder={props.placeholder}
-          onChangeText={(text) => props.setValue(text)}/>
-      <View style={styles.borderRadius}>
-
-        <TouchableOpacity
-          style={[styles.submitBtn, styles.borderRadius]}
-          onPress={() => props.inputButtonClick()}>
-          <Text style={styles.textLight}>{props.buttonText}</Text>
-        </TouchableOpacity>
-      </View>
+        placeholder={props.placeholder}
+        onChangeText={(text) => props.setValue(text)}/>
+        <View style={styles.borderRadius}>
+          <TouchableOpacity
+            style={[styles.submitBtn, styles.borderRadius]}
+            onPress={() => props.inputButtonClick()}>
+            <Text style={styles.textLight}>{props.buttonText}</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   )
 }
