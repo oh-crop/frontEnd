1cac3fd79eebfcc2fadc841d807f6110b6a5c396
Ryan Camp <campryan@comcast.net>
2020-07-28 10:24:50 -0600

    Write test for Landing Page

--- __tests__/components/pages/Landing.test.js
+++ __tests__/components/pages/Landing.test.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { render } from 'react-native-testing-library';
+import { render, cleanup } from 'react-native-testing-library';
 
 import Landing from '../../../components/pages/Landing';
 
 describe("Landing Page / Home Screen", () => {
-  let landing;
 
-  beforeAll(() => {
-    landing = render(<Landing />)
-  });
+  afterEach(cleanup);
 
-  test("it renders all inputs as expected", () => {
-    expect(landing.toJSON()).toMatchSnapshot();
+  test('it displays the logo', () => {
+    const { getByTestId } = render(<Landing />)
+
+    const image = getByTestId("Landing.Image")
+
+    expect(image.props.source.testUri).toBe('../../../assets/logo.png')
+    expect(image.type).toBe('Image')
+  })
+
+  test('renders "Search Plants" button', () => {
+    const { queryByText } = render(<Landing />)
+    expect(queryByText('Search Plants')).not.toBeNull()
+    // console.log(findByText('Search Plants').type)
+  })
+
+  test('renders "My Garden" button', () => {
+    const { queryByText } = render(<Landing />)
+    expect(queryByText('My Garden')).not.toBeNull()
+  })
+
+  test("it renders home page as expected", () => {
+    const { toJSON } = render(<Landing />)
+    expect(toJSON()).toMatchSnapshot();
   })
 })
