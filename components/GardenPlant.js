08aa0772e9ea8c7ea53918850eeb00ad0c9c0cf9
Ryan Camp <campryan@comcast.net>
2020-07-28 17:36:49 -0600

    Write test for GardenPlant

--- components/GardenPlant.js
+++ components/GardenPlant.js
@@ -5,19 +5,20 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import styles from '../styles/styles';
 
 export default function GardenPlant ({ info, navigation }) {
-  
+
   return (
     <TouchableOpacity
       style={styles.myGardenBody}
       onPress={() => {
         navigation.navigate('PlantProfilePage', {id: info.id});
-        navigation.dangerouslyGetParent().setOptions({ tabBarVisible: false });
-      }}>
+        navigation.dangerouslyGetParent().setOptions({ tabBarVisible: false })}}
+        testID="GardenPlant.TouchableOpacity">
         <View style={styles.iconContainer}>
           <MaterialCommunityIcons
             style={styles.icon}
             name="flower"
-            size={50} />
+            size={50}
+            testID="GardenPlant.Icon"/>
         </View>
         <View>
           <Text style={[styles.textLight, styles.gardenPlantText]}>{info.plant_name}</Text>
