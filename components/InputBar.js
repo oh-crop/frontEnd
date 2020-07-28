5b1b43480017a2ad4684f126f5ac94118b7de7f9
Madison Randle <madisonrandle25@gmail.com>
2020-07-28 16:08:14 -0500

    Pass styles through as props

--- components/InputBar.js
+++ components/InputBar.js
@@ -1,5 +1,6 @@
 import React, {Component, useState} from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import { View, Text, Button, TextInput,   TouchableOpacity,
+ } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import styles from '../styles/styles';
@@ -7,19 +8,20 @@ import styles from '../styles/styles';
 const searchIcon = <Ionicons name="md-search" size={40} color="black" />
 
 function InputBar (props) {
-  
+
   return (
-    <View style={styles.searchInputContainer}>
+    <View style={props.styles}>
       <TextInput
-        style={[styles.searchbox, styles.textDark]}
+        style={[styles.addNameSearchbox, styles.textDark]}
           placeholder={props.placeholder}
-          onChangeText={(text) => props.setValue(text)}
-      />
+          onChangeText={(text) => props.setValue(text)}/>
       <View style={styles.borderRadius}>
-        <Button
-          title={props.buttonText}
-          onPress={() => props.inputButtonClick()}
-        />
+
+        <TouchableOpacity
+          style={[styles.submitBtn, styles.borderRadius]}
+          onPress={() => props.inputButtonClick()}>
+          <Text style={styles.textLight}>{props.buttonText}</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
