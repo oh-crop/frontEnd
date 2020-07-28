25912a02d643aaa5fb5c586687389df812dc2b76
Ryan Camp <campryan@comcast.net>
2020-07-28 11:08:40 -0600

    Refactor and update landing page test

---
+++ __tests__/components/buttons/OpacityButton.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, cleanup } from 'react-native-testing-library';
+
+import Button from '../../../components/buttons/OpacityButton';
+
+describe("TouchableOpacity Button", () => {
+  test("test", () => {
+    expect(true).toBeTruthy()
+  })
+})
--- __tests__/components/pages/Landing.test.js
+++ __tests__/components/pages/Landing.test.js
@@ -4,31 +4,38 @@ import { render, cleanup } from 'react-native-testing-library';
 import Landing from '../../../components/pages/Landing';
 
 describe("Landing Page / Home Screen", () => {
+  let landing;
+
+  beforeEach(() => {
+    landing = render(<Landing />)
+  });
+
+  // beforeEach(() => {
+  //   landing = render(<Landing />);
+  // });
 
   afterEach(cleanup);
 
   test('it displays the logo', () => {
-    const { getByTestId } = render(<Landing />)
-
-    const image = getByTestId("Landing.Image")
+    const image = landing.getByTestId("Landing.Image")
 
     expect(image.props.source.testUri).toBe('../../../assets/logo.png')
     expect(image.type).toBe('Image')
   })
 
   test('renders "Search Plants" button', () => {
-    const { queryByText } = render(<Landing />)
-    expect(queryByText('Search Plants')).not.toBeNull()
-    // console.log(findByText('Search Plants').type)
+    const searchButton = landing.queryByText('Search Plants')
+
+    expect(searchButton).not.toBeNull()
   })
 
   test('renders "My Garden" button', () => {
-    const { queryByText } = render(<Landing />)
-    expect(queryByText('My Garden')).not.toBeNull()
+    const gardenButton = landing.queryByText('My Garden')
+
+    expect(gardenButton).not.toBeNull()
   })
 
   test("it renders home page as expected", () => {
-    const { toJSON } = render(<Landing />)
-    expect(toJSON()).toMatchSnapshot();
+    expect(landing.toJSON()).toMatchSnapshot();
   })
 })
